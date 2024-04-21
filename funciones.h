afb809aa486e3656d223b37851f75e9a3fea5a33
mmorales68 <miguelm4032@gmail.com>
2024-04-20 20:03:21 -0600

    Inicializacion del repositorio con la estructura principal

---
+++ funciones.h
@@ -0,0 +1,26 @@
+//
+// Created by mmora on 20/04/2024.
+//
+
+#ifndef MANEJO_DE_ARCHIVOS_2_0_FUNCIONES_H
+#define MANEJO_DE_ARCHIVOS_2_0_FUNCIONES_H
+
+#endif //MANEJO_DE_ARCHIVOS_2_0_FUNCIONES_H
+#include "windows.h"
+
+void carlos(){
+
+}
+
+void julio(){
+
+}
+
+void moreno(){
+
+}
+
+void miguel(){
+
+}
+
---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include "iostream"
+#include "funciones.h"
+
+using namespace std;
+
+int main() {
+
+    int select;
+
+    do{
+        cout<<"---------------------------------------------------------------------------------------------"<<endl;
+        cout<<"============================================================================================="<<endl;
+        cout<<"|| Seleccione una de las siguientes opciones:                                              ||"<<endl;
+        cout<<"||                                                                                         ||"<<endl;
+        cout<<"||     1) Funcion Carlos: Ejecuta...                                                       ||"<<endl;
+        cout<<"||     2) Funcion Julio:                                                                   ||"<<endl;
+        cout<<"||     3) Funcion Moreno:                                                                  ||"<<endl;
+        cout<<"||     4) Funcion Miguel:                                                                  ||"<<endl;
+        cout<<"||     0) Exit                                                                             ||"<<endl;
+        cout<<"||                                                                                         ||"<<endl;
+        cout<<"||                                                                                         ||"<<endl;
+        cout<<"||                                                                                         ||"<<endl;
+        cout<<"||                                                                                         ||"<<endl;
+        cout<<"||                                                                                         ||"<<endl;
+        cout<<"||                                                                                         ||"<<endl;
+        cout<<"============================================================================================="<<endl;
+        cout<<"---------------------------------------------------------------------------------------------"<<endl;
+
+        cin>>select;
+
+        switch (select){
+            case 1:
+                carlos();
+                break;
+            case 2:
+                julio();
+                break;
+            case 3:
+                moreno();
+                break;
+            case 4:
+                miguel();
+                break;
+            case 0:
+                cout<<"Saliendo del programa"<<endl;
+                system("pause");
+                break;
+            default:
+                cout<<"Opccion invalida"<<endl;
+                break;
+        }
+
+    } while (select!=0);
+
+    return 0;
+}
